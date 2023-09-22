a98782ad9e0c16a1b408b90bece7aa4613871b51
RashanWeerasinghe <malithrashan03@gmail.com>
2023-09-22 15:20:07 +0530

    Factory Method

---
+++ FactoryMethod/src/Factory.java
@@ -0,0 +1,26 @@
+import resource.Bus;
+import resource.Car;
+import resource.Van;
+import resource.Vehicle;
+
+public class Factory {
+
+    private static Factory factory=null;
+
+    private Factory(){}
+
+    public static Factory getInstance(){
+        if(factory==null) return new Factory();
+        return factory;
+    }
+    public  Vehicle getVehicle(VehicleType vehicleType){
+        switch (vehicleType){
+            case BUS:return new Bus();
+            case CAR:return new Car();
+            case VAN:return new Van();
+            default:return null;
+        }
+
+    }
+
+}
